38eed060a60ac99e7b5309f39b6b2561d2063876
7H0M45-4N70NY <thomasantony14@gmail.com>
2024-01-04 12:37:25 +0530

    data ingestion added but not tested

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Thomas!")
+
+
+STAGE_NAME="Data ingestion stage"
+
+if __name__=="__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
